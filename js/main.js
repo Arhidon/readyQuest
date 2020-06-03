ca3f7a0e8fe2c0d7226e747ec55bfe47134a78fb
Arhidon <Arhidon@users.noreply.github.com>
2020-06-03 19:26:50 +0300

    ready

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$("#phone").intlTelInput();
+$("#phone").intlTelInput({
+    utilsScript: "js/utils.js"
+});
+$("#phone").intlTelInput({
+    initialCountry: "auto",
+    geoIpLookup: function (callback) {
+        jQuery.get('https://ipinfo.io', function () { }, "jsonp").always(function (resp) {
+            var countryCode = (resp && resp.country) ? resp.country : "";
+            callback(countryCode);
+        });
+    },
+    utilsScript: "js/utils.js"
+});
